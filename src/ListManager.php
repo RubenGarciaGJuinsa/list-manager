58fbfa1c6dc9f5d8e5a70bda2b4dde92eb69c99d
Rubén García <rubengarcia@grupojuinsa.es>
2020-02-06 16:50:23 +0100

    BLUE: create_new_list_with_name

--- src/ListManager.php
+++ src/ListManager.php
@@ -10,18 +10,17 @@ class ListManager
 
     public function __construct($db)
     {
+        $db->init();
         $this->db = $db;
     }
 
     public function getLists()
     {
-        $this->db->init();
         return $this->db->getLists();
     }
 
     public function createList($name)
     {
-        $this->db->init();
         return $this->db->setList($name);
     }
 }
